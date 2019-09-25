074e29cbed397f31bedf0c1dd816091fb8a02559
Yan <850082313@qq.com>
2019-09-25 18:46:41 +0800

    commit third

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -16,6 +16,7 @@ export class ImageSliderComponent implements OnInit {
   @Input() sliders: ImageSlider[] = [];
   @ViewChild('imgSlider',{static:false}) imgSlider:ElementRef;
   @Input() intervalSeconds = 2;
+  selectedIndex = 0;
   constructor(private rd2: Renderer2) { }
 
   ngOnInit() {
@@ -28,5 +29,15 @@ export class ImageSliderComponent implements OnInit {
        (++i % this.sliders.length)*this.imgSlider.nativeElement.scrollWidth/this.sliders.length)
     },this.intervalSeconds * 1000);
   }
+
+  // handleScroll(ev){
+  //   const ratio = (ev.target.scrollLeft * this.sliders.length) / ev.target.scrollWidth;
+  //   this.selectedIndex = Math.round(ratio);
+  // }
+
+  // getIndex(idx:number): number{
+  //   return idx >=0 ? this.sliders.length
+  //   : this.sliders.length - (Math.abs(idx)%this.sliders.length);
+  // }
 
 }
