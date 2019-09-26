facea1322c2918f88440dc08429c43798138a9c3
Yan <850082313@qq.com>
2019-09-26 11:10:23 +0800

    fourth

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Renderer2, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, Renderer2, ViewChild, ElementRef, OnDestroy, AfterViewChecked, AfterViewInit } from '@angular/core';
 
 export interface ImageSlider{
   imgUrl: string,
@@ -11,12 +11,13 @@ export interface ImageSlider{
   templateUrl: './image-slider.component.html',
   styleUrls: ['./image-slider.component.scss']
 })
-export class ImageSliderComponent implements OnInit {
+export class ImageSliderComponent implements OnInit , AfterViewInit , OnDestroy{
 
   @Input() sliders: ImageSlider[] = [];
   @ViewChild('imgSlider',{static:false}) imgSlider:ElementRef;
   @Input() intervalSeconds = 2;
   selectedIndex = 0;
+  intervalId;
   constructor(private rd2: Renderer2) { }
 
   ngOnInit() {
@@ -24,20 +25,24 @@ export class ImageSliderComponent implements OnInit {
 
   ngAfterViewInit(): void {
     let i = 0;
-    setInterval(()=>{
+    this.intervalId = setInterval(()=>{
       this.rd2.setProperty(this.imgSlider.nativeElement,'scrollLeft',
-       (++i % this.sliders.length)*this.imgSlider.nativeElement.scrollWidth/this.sliders.length)
+       (this.getIndex(++this.selectedIndex) % this.sliders.length)*this.imgSlider.nativeElement.scrollWidth/this.sliders.length)
     },this.intervalSeconds * 1000);
   }
 
-  // handleScroll(ev){
-  //   const ratio = (ev.target.scrollLeft * this.sliders.length) / ev.target.scrollWidth;
-  //   this.selectedIndex = Math.round(ratio);
-  // }
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
 
-  // getIndex(idx:number): number{
-  //   return idx >=0 ? this.sliders.length
-  //   : this.sliders.length - (Math.abs(idx)%this.sliders.length);
-  // }
+  handleScroll(ev){
+    const ratio = (ev.target.scrollLeft * this.sliders.length) / ev.target.scrollWidth;
+    this.selectedIndex = Math.round(ratio);
+  }
+
+  getIndex(idx:number): number{
+    return idx >=0 ? idx % this.sliders.length
+    : this.sliders.length - (Math.abs(idx)%this.sliders.length);
+  }
 
 }
